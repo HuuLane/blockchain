65c36a8a17f472b78f0646c8243df1e001a10c1d
Huu Lane <huulane@gmail.com>
2020-10-04 11:45:03 +0800

    feat: badgerDB for persistence

--- blockchain/chain.go
+++ blockchain/chain.go
@@ -1,14 +1,40 @@
 package blockchain
 
+import (
+	"fmt"
+
+	"github.com/dgraph-io/badger/v2"
+)
+
 type BlockChain struct {
-	Blocks []*Block
+	LastHash []byte // only used for initialization
+	Database *badger.DB
 }
 
 func (c *BlockChain) AddBlock(data string) {
-	// pb: prevBlock
-	pb := c.Blocks[len(c.Blocks)-1]
-	nb := NewBlock(pb.Hash, data)
-	c.Blocks = append(c.Blocks, nb)
+	var lastHash []byte
+
+	err := c.Database.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte("lh"))
+		Handle(err)
+		lastHash, err = item.ValueCopy(lastHash)
+
+		return err
+	})
+	Handle(err)
+
+	newBlock := NewBlock(lastHash, data)
+
+	err = c.Database.Update(func(txn *badger.Txn) error {
+		err := txn.Set(newBlock.Hash, newBlock.Serialize())
+		Handle(err)
+		err = txn.Set([]byte("lh"), newBlock.Hash)
+
+		c.LastHash = newBlock.Hash
+
+		return err
+	})
+	Handle(err)
 }
 
 var (
@@ -17,7 +43,33 @@ var (
 
 func NewBlockChain() *BlockChain {
 	if singleton == nil {
-		singleton = &BlockChain{[]*Block{Genesis()}}
+		var lastHash []byte
+
+		db, err := badger.Open(badger.DefaultOptions("/tmp/badger"))
+		Handle(err)
+
+		err = db.Update(func(txn *badger.Txn) error {
+			if _, err := txn.Get([]byte("lh")); err == badger.ErrKeyNotFound {
+				fmt.Println("No existing blockchain found")
+				genesis := Genesis()
+				fmt.Println("Genesis created")
+				err = txn.Set(genesis.Hash, genesis.Serialize())
+				Handle(err)
+				err = txn.Set([]byte("lh"), genesis.Hash)
+
+				lastHash = genesis.Hash
+
+				return err
+			}
+			item, err := txn.Get([]byte("lh"))
+			Handle(err)
+			lastHash, err = item.ValueCopy(lastHash)
+			return err
+		})
+
+		Handle(err)
+
+		singleton = &BlockChain{lastHash, db}
 	}
 	return singleton
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/HuuLane/blockchain
 
 go 1.15
+
+require github.com/dgraph-io/badger/v2 v2.2007.2
